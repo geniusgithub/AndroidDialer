4b0502f966e0a52b456df467844a9a0c980332c2
wangyj20 <wangyj20@lenovo.com>
2015-11-20 17:52:29 +0800

    update project

--- code/com.android.dialer/gen/com/android/dialer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.android.dialer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- code/library/com.android.contacts.common/gen/com/android/contacts/common/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.android.contacts.common;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
